8dcfdbdec46e18a10c6c05311215d6c3cb3ed050
Michael Kühweg <developer@kuehweg.de>
2017-12-24 11:29:39 +0100

    Tap and swipe

--- Art-in-a-Box/ViewController.swift
+++ Art-in-a-Box/ViewController.swift
@@ -12,13 +12,44 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mondrianImageView: UIImageView!
     
+    @IBAction func swipeLeft(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            transitionToNewImage(image: randomImage(), transition: .transitionFlipFromRight)
+        }
+    }
+    
+    @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            transitionToNewImage(image: randomImage(), transition: .transitionCurlUp)
+        }
+    }
+    
+    @IBAction func tapOnImage(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            transitionToNewImage(image: randomImage())
+        }
+    }
+    
     @IBAction func refreshImage(_ sender: UIBarButtonItem) {
+        transitionToNewImage(image: randomImage())
+    }
+    
+    private func randomImage() -> UIImage {
         let creativeIdea = Mondrian()
         creativeIdea.randomArtwork()
         // images are supposed to possibly serve as background images
         let canvasSize = max(mondrianImageView.frame.width, mondrianImageView.frame.height)
         let art = MondrianOnCanvas(mondrian: creativeIdea, canvasSize: Int(canvasSize))
-        mondrianImageView.image = art.asImage()
+        return art.asImage()
+    }
+    
+    private func transitionToNewImage(image: UIImage,
+                                      transition: UIViewAnimationOptions = .transitionCrossDissolve) {
+        UIView.transition(with: mondrianImageView,
+                          duration: 1,
+                          options: transition,
+                          animations: { self.mondrianImageView.image = image },
+                          completion: nil)
     }
     
     @IBAction func shareImage(_ sender: UIBarButtonItem) {
